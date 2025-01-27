5cdb428085546a05550dae51f19d29135c1a0325
권용규 <@LAPTOP-H0FP6QDH>
2025-01-27 23:41:43 +0900

    [KYK] 조건문 과제

--- lec03_control/src/com/gn/homework/loop/Practice.java
+++ lec03_control/src/com/gn/homework/loop/Practice.java
@@ -1,5 +1,52 @@
 package com.gn.homework.loop;
 
+import java.util.Scanner;
+
 public class Practice {
+	
+	public void practice02() {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("숫자 : ");
+			int a = sc.nextInt();
+			if(a >= 10) {
+				System.out.println("9 이하의 숫자만 입력해주세요"); continue;
+			}else if(a < 0){
+				System.out.println("양수만 입력해주세요"); continue;
+			}else if(a == 0) {
+				System.out.println("프로그램 종료!"); 
+				break;
+			}else {
+				for(int i = a ; i <= a ; i++) {
+					System.out.println("===" + a + "단===");
+					for(int j = 1 ; j <= 9 ; j++){
+						System.out.println(a + "*" + j + "=" + a*j);
+					}
+				}
+				break;
+			}
+		}		
+	}
+	
+	public void practice03() {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("입력 : ");
+			String a = sc.nextLine();
+			System.out.println(a); 
+			if(a.equals("탈출")) {
+				System.out.println("프로그램 종료");
+				break;
+			}
+		}
+
+
+	}
+	
+	public void practice07() {
+		
+	}
 
 }
--- lec03_control/src/com/gn/homework/loop/Run.java
+++ lec03_control/src/com/gn/homework/loop/Run.java
@@ -1,9 +1,25 @@
 package com.gn.homework.loop;
 
+import java.util.Scanner;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Practice a = new Practice();
+		
+		System.out.println("2.구구단 무한반복");
+		System.out.println("3.탈출합시다");
+		System.out.println("6.트리 만들기");
+		System.out.println("7.박수박수박");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("선택 : ");
+		int a1 = sc.nextInt();
+		switch(a1) {
+		case 2 : a.practice02(); break;
+		case 3 : a.practice03(); break;
+		}
+		
 
 	}
 
